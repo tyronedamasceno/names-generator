016eb3c125a9c823379d7148fed837fa68caba16
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-10-31 16:16:56 -0300

    Iterate with v-for

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,12 @@
       <div class="container">
         <div class="row">
           <div class="col-md">
-            <h5>Prefixos <span class="badge badge-info">0</span></h5>
+            <h5>Prefixos <span class="badge badge-info">{{prefixes.length}}</span></h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li class="list-group-item">
-                    A
-                  </li>
-                  <li class="list-group-item">
-                    B
-                  </li>
-                  <li class="list-group-item">
-                    C
+                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                    {{ prefix }}
                   </li>
                 </ul>
                 <br/>
@@ -29,24 +23,29 @@
             </div>
           </div>
           <div class="col-md">
-            <h5>Sufixos <span class="badge badge-info">0</span></h5>
+            <h5>Sufixos <span class="badge badge-info">{{ sufixes.length }}</span></h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li class="list-group-item">
-                    A
-                  </li>
-                  <li class="list-group-item">
-                    B
-                  </li>
-                  <li class="list-group-item">
-                    C
+                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                    {{ sufix }}
                   </li>
                 </ul>
                 <br/>
                 <input type="text" class="form-control" placeholder="Digite o sufixo">
               </div>
             </div>
+          </div>
+        </div>
+        <br/>
+        <h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+        <div class="card">
+          <div class="card-body">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
+                {{ domain }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -60,6 +59,13 @@ import "font-awesome/css/font-awesome.css"
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      prefixes: ['Air', 'Jet', 'Flight'],
+      sufixes: ['Hub', 'Station', 'Mart'],
+      domains: ['AirHub', 'AirStation', 'AirMart', 'JetHub', 'JetStation', 'JetMart', 'FlightHub', 'FlightStation', 'FlightMart']
+    };
+  }
 }
 </script>
 
